a2d43cdfc54496f38b23b2733f3476d6efc1bc7a
Gabriel Vanca <gabriel@vanca.co.uk>
2023-08-23 01:11:54 +0100

    arguments renamed

--- Chocolatey_Deploy.bat
+++ Chocolatey_Deploy.bat
@@ -6,10 +6,11 @@
 ::  Place Arguments here  ::
 ::::::::::::::::::::::::::::
 
-SET arg1=$True
-SET arg2="http://hercules.cerberus.local:8624/nuget/Hercules/"
-SET arg3="HERCULES"
-SET arg4=$False
+
+SET LocalRepository=$True
+SET LocalRepositoryPath="http://hercules.cerberus.local:8624/nuget/Hercules/"
+SET LocalRepositoryName="HERCULES"
+SET DisableCommunityRepository=$False
 
 
 ::::::::::::::::::::::::::::
@@ -17,5 +18,5 @@ SET arg4=$False
 ::::::::::::::::::::::::::::
 
 
-Powershell -ExecutionPolicy Bypass -Command "& {$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent()); if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) -eq $true) {Write-Host "This session is running with Administrator priviledges." -ForegroundColor DarkGreen} else {Write-Host "This session is not running with Administrator priviledges." -ForegroundColor DarkRed; $Host.UI.RawUI.WindowTitle = '[Not Admin]: ' + $host.UI.RawUI.WindowTitle; Write-Host "Please close this prompt and restart as admin" -ForegroundColor DarkRed; Write-Host "Press any key to exit..."; $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown'); throw 'Session is not running with Administrator priviledges.';}; $scriptPath = 'https://raw.githubusercontent.com/gabriel-vanca/Chocolatey/main/Chocolatey_Deploy.ps1'; $WebClient = New-Object Net.WebClient;$deploymentScript = $WebClient.DownloadString($scriptPath); $deploymentScript = [Scriptblock]::Create($deploymentScript); Invoke-Command -ScriptBlock $deploymentScript -ArgumentList (%arg1%, '%arg2%', '%arg3%', %arg4%) -NoNewScope; Write-Host 'Press any key to exit...';$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')}" -Verb RunAs -noexit
+Powershell -ExecutionPolicy Bypass -Command "& {$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent()); if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) -eq $true) {Write-Host "This session is running with Administrator priviledges." -ForegroundColor DarkGreen} else {Write-Host "This session is not running with Administrator priviledges." -ForegroundColor DarkRed; $Host.UI.RawUI.WindowTitle = '[Not Admin]: ' + $host.UI.RawUI.WindowTitle; Write-Host "Please close this prompt and restart as admin" -ForegroundColor DarkRed; Write-Host "Press any key to exit..."; $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown'); throw 'Session is not running with Administrator priviledges.';}; $scriptPath = 'https://raw.githubusercontent.com/gabriel-vanca/Chocolatey/main/Chocolatey_Deploy.ps1'; $WebClient = New-Object Net.WebClient;$deploymentScript = $WebClient.DownloadString($scriptPath); $deploymentScript = [Scriptblock]::Create($deploymentScript); Invoke-Command -ScriptBlock $deploymentScript -ArgumentList (%LocalRepository%, '%LocalRepositoryPath%', '%LocalRepositoryName%', %DisableCommunityRepository%) -NoNewScope; Write-Host 'Press any key to exit...';$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')}" -Verb RunAs -noexit
 EXIT /B
